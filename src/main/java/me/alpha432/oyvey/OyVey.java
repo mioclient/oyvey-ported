a13303ba6b29b565427de9b16fbaa56174a78d59
cattyn <cvenby@gmail.com>
2024-08-15 23:42:12 +0300

    bussin

--- src/main/java/me/alpha432/oyvey/OyVey.java
+++ src/main/java/me/alpha432/oyvey/OyVey.java
@@ -8,7 +8,7 @@ import org.apache.logging.log4j.Logger;
 
 public class OyVey implements ModInitializer, ClientModInitializer {
     public static final String NAME = "OyVey";
-    public static final String VERSION = "0.0.3 - 1.20.1";
+    public static final String VERSION = "0.0.3 - 1.21";
 
     public static float TIMER = 1f;
 
