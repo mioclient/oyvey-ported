9792fd0ed1df58310dd53682fece2e1523527056
cattyn <cvenby@gmail.com>
2025-01-26 23:46:25 +0300

    auto version bump

--- src/main/java/me/alpha432/oyvey/OyVey.java
+++ src/main/java/me/alpha432/oyvey/OyVey.java
@@ -3,12 +3,13 @@ package me.alpha432.oyvey;
 import me.alpha432.oyvey.manager.*;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.SharedConstants;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class OyVey implements ModInitializer, ClientModInitializer {
     public static final String NAME = "OyVey";
-    public static final String VERSION = "0.0.3 - 1.21.1";
+    public static final String VERSION = "0.0.3 - " + SharedConstants.getGameVersion().getName();
 
     public static float TIMER = 1f;
 
