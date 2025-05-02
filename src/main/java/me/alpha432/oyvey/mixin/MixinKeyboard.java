e83733e145197c473997d1932f24e1b000bbf34e
cattyn <cvenby@gmail.com>
2025-05-02 19:53:56 +0300

    fixed bind bug

--- src/main/java/me/alpha432/oyvey/mixin/MixinKeyboard.java
+++ src/main/java/me/alpha432/oyvey/mixin/MixinKeyboard.java
@@ -13,6 +13,7 @@ import static me.alpha432.oyvey.util.traits.Util.EVENT_BUS;
 public class MixinKeyboard {
     @Inject(method = "onKey", at = @At("TAIL"), cancellable = true)
     private void onKey(long windowPointer, int key, int scanCode, int action, int modifiers, CallbackInfo ci) {
+        if (action != 1) return;
         KeyEvent event = new KeyEvent(key);
         EVENT_BUS.post(event);
         if (event.isCancelled()) ci.cancel();
