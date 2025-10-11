a94978e5048da92ee2971245e2308e1710cb5b7e
0x12F <atav44@outlook.com>
2025-10-11 14:44:03 +0300

    cleanup

--- src/main/java/me/alpha432/oyvey/features/modules/hud/Coordinates.java
+++ src/main/java/me/alpha432/oyvey/features/modules/hud/Coordinates.java
@@ -1,7 +1,8 @@
-package me.alpha432.oyvey.features.modules.client;
+package me.alpha432.oyvey.features.modules.hud;
 
 import com.google.common.eventbus.Subscribe;
 import me.alpha432.oyvey.event.impl.Render2DEvent;
+import me.alpha432.oyvey.features.modules.client.HudModule;
 import me.alpha432.oyvey.features.settings.Setting;
 
 public class Coordinates extends HudModule {
